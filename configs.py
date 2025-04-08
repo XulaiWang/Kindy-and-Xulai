a53dddcf4c5b64f2d4e25f2cbd92d37087fbb9a9
Kindy27 <kgan1@jh.edu>
2025-04-07 23:36:09 -0400

    read and save path

--- configs.py
+++ configs.py
@@ -1,3 +1,5 @@
 class DefaultConfig(object):
     path = './lazor_fall_2024/bff_files/yarn_5.bff'
     save_path = './saved/yarn_5.png'
+
+** Pls just change the bff file name to get the solution to different games
